4c510bbbf6300ee5d33211db729f1a9507f726fb
idontknowhowtocode3 <156248506+idontknowhowtocode3@users.noreply.github.com>
2025-11-15 15:33:44 -0500

    Update catgame.py
    Chloe's started code with pygame

--- catgame.py
+++ catgame.py
@@ -1,1 +1,37 @@
+import pygame
+
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Fat Cat Dash")
+
+
+def first_House():
+    BG = pygame.image.load("Background.jpg")
+    WINDOW.blit(BG, (0,0))
+    pygame.display.update()
+
+
+def main():
+    running = True
+
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+       
+        first_House()
+
+
+
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
 
